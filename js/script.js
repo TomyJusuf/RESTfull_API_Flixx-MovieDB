de74e7cd51c87c0d255b122332329d91e594ecb5
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-09-16 15:46:52 +0200

    Implement GET method to fetching data from the API with error handling. Added functionality  to render data to HTML structure, for movie-details.html, show.html, tv-details.html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 const tokey =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTQwMGM4N2FmZTEyMzkzZGMyZTM1YzUwNDQwM2JkMCIsIm5iZiI6MTcyNTg2MTQ3OS42NjQzNTYsInN1YiI6IjY0OWIyM2I3MGU1YWJhMDBjNTkxYWUzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._raW47XBufu1iIJ5yMMzzPt51cTtPOWSkLPOkfOhGdA'
-const key = 'f5400c87afe12393dc2e35c504403bd0'
-let urlLink = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&page=1`
-const id = window.location.search
+
+// const id = window.location.search
 
 const options = {
   method: 'GET',
@@ -20,7 +19,7 @@ const languages = {
   it: 'it-IT',
   chi: 'zh-CN',
 }
-
+const baseImageUrl = 'https://image.tmdb.org/t/p/w500' // base URL for images
 async function fetchData(url, opt) {
   try {
     const response = await fetch(url, opt)
@@ -28,7 +27,6 @@ async function fetchData(url, opt) {
       throw new Error(`HTTP error! status: ${response.status}`)
     }
     const data = await response.json()
-    // console.log(data.results)
 
     renderMovies(data.results)
   } catch (error) {
@@ -58,13 +56,12 @@ function getLanguage(e) {
     popularMovies.innerHTML = ''
   }
   if (languageCode) {
-    urlLink = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=${languageCode}&page=1`
-    fetchData(urlLink)
+    urlLink = `https://api.themoviedb.org/3/movie/popular?language=${languageCode}&page=1`
+    fetchData(urlLink, options)
   }
 }
 
 // Render movies
-const baseImageUrl = 'https://image.tmdb.org/t/p/w500' // base URL for images
 
 function renderMovies(movies) {
   movies.forEach((movie) => {
@@ -109,6 +106,7 @@ function renderMovieDetails(movie) {
     genres,
     production_companies,
   } = movie
+
   const fullPosterUrl = `${baseImageUrl}${poster_path}` // Construct full image URL;
   function loopGenres(genres) {
     let genreList = ''
@@ -117,28 +115,12 @@ function renderMovieDetails(movie) {
     })
     return genreList
   }
-  function productionCompanies(Companies) {
+  function productionCompanies(companies) {
     let companyList = ''
-    Companies.forEach((company) => {
+    companies.forEach((company) => {
       companyList += `  <div class="list-group">${company.name} </div>`
     })
     return companyList
-  }
-  function voteRating(vote_average) {
-    const rating = Math.round(vote_average)
-    const calcAverage = (rating / 10) * 5
-
-    const fullStar = Math.round(calcAverage)
-    let ratingHTML = ''
-    for (let i = 0; i < 5; i++) {
-      if (i < fullStar) {
-        ratingHTML += `<i class="fas fa-star text-primary"></i>
-        `
-      } else {
-        ratingHTML += `<i class="far fa-star text-primary"></i>`
-      }
-    }
-    return ratingHTML
   }
 
   movieDetails.innerHTML = `
@@ -153,11 +135,12 @@ function renderMovieDetails(movie) {
           <div>
             <h2>${title}</h2>
             <p>
-             ${voteRating(vote_average)} 
+           <i class="fas fa-star text-primary"></i>
+           ${Math.round(vote_average)} /10 
             </p>
             <p class="text-muted">Release Date: ${release_date}</p>
             <p>
-              ${overview}
+              ${overview ? overview : 'No overview available'}
             </p>
             <h5>Genres</h5>
             <ul class="list-group">
@@ -169,8 +152,9 @@ function renderMovieDetails(movie) {
         <div class="details-bottom">
           <h2>Movie Info</h2>
           <ul>
-            <li><span class="text-secondary">Budget:</span> $ ${budget}</li>
-            <li><span class="text-secondary">Revenue:</span> $ ${revenue}</li>
+            <li><span class="text-secondary">Budget:</span> $ ${budget.toLocaleString()}</li>
+
+            <li><span class="text-secondary">Revenue:</span> $ ${revenue.toLocaleString()}</li>
             <li><span class="text-secondary">Runtime:</span> ${runtime} minutes</li>
             <li><span class="text-secondary">Status:</span> ${status}</li>
           </ul>
@@ -182,29 +166,38 @@ function renderMovieDetails(movie) {
   `
 }
 
-function detailMovie() {
+async function detailMovie() {
   const queryString = window.location.search
   const id = queryString.split('=')[1]
   const lang = localStorage.getItem('lang')
 
   const languageCode = languages[lang]
+  const fallbackLanguage = 'en-Us'
 
-  const url = `https://api.themoviedb.org/3/movie/${id}?language=${languageCode}&api_key=${key}`
-
-  async function fetchData(url, opt) {
+  async function fetchData(lang) {
+    const url = `https://api.themoviedb.org/3/movie/${id}?language=${
+      lang ? lang : languageCode
+    }`
     try {
-      const response = await fetch(url, opt)
+      const response = await fetch(url, options)
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`)
       }
-      const data = await response.json()
-      renderMovieDetails(data)
+      return await response.json()
     } catch (error) {
       console.log(error)
     }
   }
+  let data = await fetchData()
 
-  fetchData(url)
+  if (!data.overview) {
+    console.log(
+      `No overview in ${languageCode}, fetching in fallback language (${fallbackLanguage})...`
+    )
+    data = await fetchData(fallbackLanguage)
+  }
+  renderMovieDetails(data)
+  fetchData()
 }
 
 // Highlight active link based on the link name
@@ -217,6 +210,152 @@ function highlightActiveLink(linkName) {
       link.classList.remove('active') // Clear active class from other links
     }
   })
+}
+
+function getTvShows() {
+  const lang = localStorage.getItem('lang')
+
+  const languageCode = languages[lang]
+  const url = `https://api.themoviedb.org/3/tv/popular?language=${languageCode}`
+  async function fetchData() {
+    try {
+      const response = await fetch(url, options)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const data = await response.json()
+      renderTvShows(data.results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  fetchData()
+}
+
+function renderTvShows(data) {
+  data.forEach((movie) => {
+    const { poster_path, name, first_air_date, id } = movie
+    const populatTvShows = document.getElementById('popular-shows')
+    const oneMovie = `
+    <div class="card">
+      <a href="tv-details.html?id=${id}">
+      <img src="${
+        poster_path ? `${baseImageUrl}${poster_path}` : 'images/no-image.jpg'
+      }"
+      class="card-img-top"
+      alt="${name || 'images/no-image.jpg'}"/>
+      </a>
+      <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text">
+          <small class="text-muted">Aired: ${first_air_date}</small>
+      </p>
+      </div>
+    </div>
+`
+    populatTvShows.innerHTML += oneMovie
+  })
+}
+
+async function getDetailsTVShow() {
+  const idTvShow = window.location.search.split('=')[1]
+  const lang = localStorage.getItem('lang')
+  const languageCode = languages[lang]
+  const fallbackLanguage = 'en-US'
+
+  async function fetchData(lang) {
+    const url = `https://api.themoviedb.org/3/tv/${idTvShow}?language=${
+      lang ? lang : languageCode
+    }`
+    try {
+      const response = await fetch(url, options)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      return await response.json()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  let data = await fetchData(languageCode)
+
+  // If the overview is missing or empty, fetch again with fallback language
+  if (!data.overview) {
+    console.log(
+      `No overview in ${languageCode}, fetching in fallback language (${fallbackLanguage})...`
+    )
+    data = await fetchData(fallbackLanguage)
+  }
+
+  renderDetailsTVShow(data)
+}
+
+function renderDetailsTVShow(data) {
+  const {
+    name,
+    overview,
+    first_air_date,
+    last_episode_to_air,
+    status,
+    genres,
+    poster_path,
+    vote_average,
+    homepage,
+    production_companies,
+    episode_run_time,
+  } = data
+
+  const showDetails = document.getElementById('show-details')
+  const details = `
+     <div class="details-top">
+          <div>
+            <img
+              src="${baseImageUrl}${poster_path}"
+              class="card-img-top"
+              alt="${name || 'images/no-image.jpg'}"
+            />
+          </div>
+          <div>
+            <h2>${name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${Math.round(vote_average)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${first_air_date}</p>
+            <p>
+             ${
+               overview ? overview : 'No available translation in this language'
+             }
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+             ${genres.map((genre) => `<li>${genre.name}</li>`).join(' ')}
+            </ul>
+            <a href="${homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${
+              episode_run_time[0] || 'N/A'
+            }</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${
+                last_episode_to_air.air_date || 'N/A'
+              }
+            </li>
+            <li><span class="text-secondary">Status:</span> ${status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${production_companies
+            .map((companies) => `<p>${companies.name}</p>`)
+            .join(' ')}</div>
+        </div>
+
+  `
+  showDetails.innerHTML += details
 }
 
 // Initialize the app
@@ -236,6 +375,11 @@ function init() {
     case '/shows.html':
     case '/tv-details.html':
       highlightActiveLink('TV Shows')
+      if (pathName === '/shows.html') {
+        getTvShows()
+      } else {
+        getDetailsTVShow()
+      }
       break
     case '/search.html':
       break
